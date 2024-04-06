46aef7be40aae4da6d7729219d0ceec4627c37fc
Matheus Lopes <matheus.lopes23@hotmail.com>
2024-04-05 23:46:05 -0300

    first commit

---
+++ src/main/kotlin/com/mvclopes/transfersystem/TransferSystemApplication.kt
@@ -0,0 +1,11 @@
+package com.mvclopes.transfersystem
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TransferSystemApplication
+
+fun main(args: Array<String>) {
+	runApplication<TransferSystemApplication>(*args)
+}
---
+++ src/test/kotlin/com/mvclopes/transfersystem/TransferSystemApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.mvclopes.transfersystem
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TransferSystemApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
